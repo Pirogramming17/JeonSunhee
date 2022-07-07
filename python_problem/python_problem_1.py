65316e52761d66b2e0bafd4686775fd4b1f882c6
sunny <rosa7536@naver.com>
2022-07-07 14:33:13 +0900

    step 4

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -19,3 +19,8 @@ while (isValid(cnt) != 1):
         print('1,2,3 중 하나를 입력하세요')
     cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
+cnt = int(cnt)
+
+for i in range(cnt):
+    num+=1
+    print("PlayerA :", num)
