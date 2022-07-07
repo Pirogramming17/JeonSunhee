a5fdce1d0b4a188e42743d6260304d078a2c60ff
sunny <rosa7536@naver.com>
2022-07-07 14:47:07 +0900

    step 6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,5 @@
 num = 0
 
-cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
 # 입력값 유효성 검사 함수
 def isValid(x):
@@ -11,32 +10,46 @@ def isValid(x):
         return 2
     except ValueError:
         return 3
-# 제대로 입력받을 때까지 반복
-while (isValid(cnt) != 1):
-    if isValid(cnt) == 3:
-        print('정수를 입력하세요')
-    else:
-        print('1,2,3 중 하나를 입력하세요')
+
+
+while num<31:
+
     cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-cnt = int(cnt)
+    # 제대로 입력받을 때까지 반복
+    while (isValid(cnt) != 1):
+        if isValid(cnt) == 3:
+            print('정수를 입력하세요')
+        else:
+            print('1,2,3 중 하나를 입력하세요')
+        cnt = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-for i in range(cnt):
-    num+=1
-    print("PlayerA :", num)
+    cnt = int(cnt)
 
-cnt2 = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
-
-# 제대로 입력받을 때까지 반복
-while (isValid(cnt2) != 1):
-    if isValid(cnt2) == 3:
-        print('정수를 입력하세요')
-    else:
-        print('1,2,3 중 하나를 입력하세요')
+    for i in range(cnt):
+        num+=1
+        print("PlayerA :", num)
+        if num == 31:
+            break
+    if num == 31:
+            break        
+    # Player B 부분
     cnt2 = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-cnt2 = int(cnt2)
+    # 제대로 입력받을 때까지 반복
+    while (isValid(cnt2) != 1):
+        if isValid(cnt2) == 3:
+            print('정수를 입력하세요')
+        else:
+            print('1,2,3 중 하나를 입력하세요')
+        cnt2 = input('부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :')
 
-for i in range(cnt2):
-    num+=1
-    print("PlayerB :", num)
+    cnt2 = int(cnt2)
+
+    for i in range(cnt2):
+        num+=1
+        print("PlayerB :", num)
+        if num == 31:
+            break
+    if num == 31:
+            break        
