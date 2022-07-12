2b0c20d93d402cc58041d3478c28749a85835094
sunny <rosa7536@naver.com>
2022-07-12 21:38:18 +0900

    ~ 나의 첫 번째 Django 프로젝트!

---
+++ djangogirls/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\developer\piro17_hw\jeonsunhee\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
