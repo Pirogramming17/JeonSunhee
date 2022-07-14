6b7779ef38cfa8b275683963db9374881c1d6560
sunny <rosa7536@naver.com>
2022-07-14 23:46:35 +0900

    README.md 추가

--- myMovieReviews/posts/urls.py
+++ myMovieReviews/posts/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('create', views.create, name="create"),
     path('post/<int:id>', views.detail, name="detail"),
     path('update/<int:id>', views.update, name="update"),
-    path('delete/<int:id>', views.delete, name="delete")
+    path('delete/<int:id>', views.delete, name="delete"),
 ]
