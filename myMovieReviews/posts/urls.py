c0be7dc76e81e928820902bf3110da2ed9c5fc27
sunny <rosa7536@naver.com>
2022-07-14 23:35:25 +0900

    수정하기

--- myMovieReviews/posts/urls.py
+++ myMovieReviews/posts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.home, name="home"),
     path('create', views.create, name="create"),
     path('post/<int:id>', views.detail, name="detail"),
+    path('update/<int:id>', views.update, name="update"),
 ]
--- myMovieReviews/posts/views.py
+++ myMovieReviews/posts/views.py
@@ -37,3 +37,23 @@ def detail(request, id):
     }
     return render(request, template_name="posts/detail.html", context=context)
 
+def update(request, id):
+    if request.method == 'POST':
+        title = request.POST["title"]
+        year = request.POST["year"]
+        actor = request.POST["actor"]
+        genre = request.POST["genre"]
+        star = request.POST["star"]
+        review = request.POST["review"]
+        director = request.POST["director"]
+        running = request.POST["running"]
+
+        Post.objects.filter(id=id).update(title=title, year=year, actor=actor, genre=genre, star=star, review=review, director=director, running=running)
+        return redirect(f"/post/{id}")
+    
+    post = Post.objects.get(id=id)
+    context = {
+        "post":post
+    }
+    return render(request, template_name="posts/update.html", context=context)
+    
